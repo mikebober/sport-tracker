834e2ebe47b346ea2ce32f2d029424e52d2ac12a
mikebober <mikebober6@gmail.com>
2016-10-15 20:28:07 -0400

    inital commit

---
+++ client/routes.js
@@ -0,0 +1,209 @@
+FlowRouter.route('/',{
+  name:'Home',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Home'});
+  }
+})
+FlowRouter.route('/page1',{
+  name:'Page1',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Page1'});
+  }
+})
+FlowRouter.route('/page2',{
+  name:'Page2',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Page2'});
+  }
+})
+FlowRouter.route('/page3',{
+  name:'Page3',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Page3'});
+  }
+})
+//end of nav bar
+
+//start of team pages
+FlowRouter.route('/bsoccer',{
+  name:'bsoccer',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Bsoccer'});
+  }
+})
+FlowRouter.route('/gsoccer',{
+  name:'gsoccer',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'gsoccer'});
+  }
+})
+FlowRouter.route('/football',{
+  name:'football',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Football'});
+  }
+})
+FlowRouter.route('/fieldhockey',{
+  name:'fieldhockey',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Fieldhockey'});
+  }
+})
+FlowRouter.route('/volleyball',{
+  name:'volleyball',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Volleyball'});
+  }
+})
+FlowRouter.route('/gcrosscountry',{
+  name:'gcrosscountry',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Gcrosscountry'});
+  }
+})
+FlowRouter.route('/bcrosscountry',{
+  name:'bcrosscountry',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Bcrosscountry'});
+  }
+})
+FlowRouter.route('/bbasketball',{
+  name:'bbasketball',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Bbasketball'});
+  }
+})
+FlowRouter.route('/gbasketball',{
+  name:'gbasketball',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Gbasketball'});
+  }
+})
+FlowRouter.route('/bindoortrack',{
+  name:'bindoortrack',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Bindoortrack'});
+  }
+})
+FlowRouter.route('/gindoortrack',{
+  name:'gindoortrack',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Gindoortrack'});
+  }
+})
+FlowRouter.route('/gswimming',{
+  name:'gswimming',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Gswimming'});
+  }
+})
+FlowRouter.route('/bswimming',{
+  name:'bswimming',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Bswimming'});
+  }
+})
+FlowRouter.route('/hockey',{
+  name:'hockey',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Hockey'});
+  }
+})
+FlowRouter.route('/wrestiling',{
+  name:'wrestiling',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Wrestiling'});
+  }
+})
+FlowRouter.route('/baseball',{
+  name:'baseball',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Baseball'});
+  }
+})
+FlowRouter.route('/softball',{
+  name:'softball',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Softball'});
+  }
+})
+FlowRouter.route('/goutdoortrack',{
+  name:'goutdoortrack',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Goutdoortrack'});
+  }
+})
+FlowRouter.route('/boutdoortrack',{
+  name:'boutdoortrack',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Boutdoortrack'});
+  }
+})
+FlowRouter.route('/golf',{
+  name:'golf',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Golf'});
+  }
+})
+FlowRouter.route('/glacrosse',{
+  name:'glacrosse',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Glacrosse'});
+  }
+})
+FlowRouter.route('/blacrosse',{
+  name:'blacrosse',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Blacrosse'});
+  }
+})
+FlowRouter.route('/btennis',{
+  name:'btennis',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Btennis'});
+  }
+})
+FlowRouter.route('/gtennis',{
+  name:'gtennis',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Gtennis'});
+  }
+})
+//end of team pages
+
+//start of player stat pages
+FlowRouter.route('/gsoccerplayers',{
+  name:'gsoccerplayers',
+  action(){
+
+    BlazeLayout.render('MainLayout',{main:'Gsoccerplayers'});
+  }
+})
