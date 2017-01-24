4904c62a704e9aa418dcbead6e2018610f11b4ee
mikebober <mikebober6@gmail.com>
2017-01-23 19:40:24 -0500

    commit
    updated data

--- imports/ui/views/bsoccer.js
+++ imports/ui/views/bsoccer.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Players } from '../../api/players/players.js';
+import { Teams } from '../../api/teams/teams.js';
 
 
 Template.Bsoccer.onCreated(function (){
@@ -11,6 +12,20 @@ Template.Bsoccer.onCreated(function (){
 Template.Bsoccer.helpers({
     players() {
         return Players.find({
+            sport:"soccer"
+        });
+    }
+})
+
+Template.Bsoccer.onCreated(function (){
+  this.autorun(() => {
+    this.subscribe('teams.public');
+  })
+});
+
+Template.Bsoccer.helpers({
+    teams() {
+        return Teams.find({
             sport:"soccer"
         });
     }
--- imports/ui/views/football.js
+++ imports/ui/views/football.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { Players } from '../../api/players/players.js';
-
+import { Teams } from '../../api/teams/teams.js';
 
 Template.Football.onCreated(function (){
   this.autorun(() => {
@@ -11,6 +11,20 @@ Template.Football.onCreated(function (){
 Template.Football.helpers({
     players() {
         return Players.find({
+            sport:"football"
+        });
+    }
+})
+
+Template.Football.onCreated(function (){
+  this.autorun(() => {
+    this.subscribe('teams.public');
+  })
+});
+
+Template.Football.helpers({
+    teams() {
+        return Teams.find({
             sport:"football"
         });
     }
